e3cb29fc7a3c31bf0700d2ee12decc14e88dfa9b
narekmouse <narek.mouse@gmail.com>
2018-06-17 09:07:29 +0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='MagicCubeLib2',
+      version='2.1-Fusion',
+      description='Magic Cube library | Designed to solve the problem of the Rubik\'s cube',
+      author='Narek Mouse',
+      author_email='narek.mouse@gmail.com',
+      url='https://github.com/MagicCubeProject/MagicCubeLib2',
+      packages=find_packages(),
+     )
